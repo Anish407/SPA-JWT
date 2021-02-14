7a527dc19b51dbeab24653db21373b9b41ac71dd
Anish Aravind <anish.arvind407@gmail.com>
2021-02-14 23:11:43 +0530

    Revert "demo comment added"
    This reverts commit 42dd666cb45dfafcd1de9a9aab07960b74f2c77f.

--- Core5Ng.Common/Constants/Roles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core5Ng.Common.Constants
-{
-    public class Roles
-    {
-        public const string Admin  = "Administrator";
-        public const  string Customer = "Customer";
-    }
-}
